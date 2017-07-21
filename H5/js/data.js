f2dfd80e88110b5289236ce25a0a53d66b165657
liuwei <lw5116@163.com>
2017-07-21 10:58:56 +0800

    init

---
+++ H5/js/data.js
@@ -0,0 +1,63 @@
+
+var showData = [
+					{
+						id:1,
+						photo:[
+								{
+									url:"images/photo/photo01.jpg",
+									size:"1280x720"
+
+								},
+								{
+									url:"images/photo/photo02.jpg",
+									size:"1280x720"
+
+								},	
+								{
+									url:"images/photo/photo03.jpg",
+									size:"1920x1200"
+
+								},	
+
+								{
+									url:"images/photo/photo04.jpg",
+									size:"1280x800"
+
+								},	
+
+								{
+									url:"images/photo/photo05.jpg",
+									size:"1280x800"
+
+								},	
+
+								{
+									url:"images/photo/photo06.jpg",
+									size:"1920x1080"
+
+								},
+
+								{
+									url:"images/photo/photo07.jpg",
+									size:"1080x1920"
+
+								},
+
+								{
+									url:"images/photo/photo08.jpg",
+									size:"1080x1920"
+
+								},	
+								{
+									url:"images/photo/photo09.jpg",
+									size:"1920x1098"
+
+								},	
+								{
+									url:"images/photo/photo10.jpg",
+									size:"1920x1080"
+
+								}
+						]
+					},								
+			 ];
---
+++ js/data.js
@@ -0,0 +1,25 @@
+
+var showData = [
+					{
+						id:1,
+						photo:[
+								"images/photo/photo01.jpg",
+								"images/photo/photo02.jpg",
+								"images/photo/photo03.jpg",
+								"images/photo/photo04.jpg",
+								"images/photo/photo05.jpg",
+								"images/photo/photo06.jpg",
+								"images/photo/photo07.jpg",
+								"images/photo/photo08.jpg",
+								"images/photo/photo09.jpg",
+								"images/photo/photo10.jpg",
+								"images/photo/photo11.jpg",
+								"images/photo/photo12.jpg",
+								"images/photo/photo13.jpg",
+								"images/photo/photo14.jpg",
+								"images/photo/photo15.jpg",
+								"images/photo/photo16.jpg",
+								"images/photo/photo17.jpg",
+						]
+					},								
+			 ];
